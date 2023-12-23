7c7ece69b29185ca834b04799d2ae69e6dabf828
LULIU-9 <lu.liu4work@gmail.com>
2023-12-23 12:40:07 +1000

    chore: update .gitignore to exclude sensitive information

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://zflbvnaexhhwvdpfbtoe.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpmbGJ2bmFleGhod3ZkcGZidG9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMwNTQ5MTksImV4cCI6MjAxODYzMDkxOX0.rXlSIRd4WiR4lFX-dJmuhWW69sgM2H7TdCTvOw_gD5g";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);

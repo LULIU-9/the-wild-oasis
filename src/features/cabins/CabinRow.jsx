0ae4561344c8e49ba6e047f611024a1fc9de8bbe
LULIU-9 <lu.liu4work@gmail.com>
2023-12-23 13:42:11 +1000

    feat: configure and integrate react-hot-toast for notifications

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabin";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -59,9 +60,9 @@ function CabinRow({ cabin }) {
       queryClient.invalidateQueries({
         queryKey: ["cabin"],
       });
-      alert("Cabin successfully deleted");
+      toast.success("Cabin successfully deleted");
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
 
   return (

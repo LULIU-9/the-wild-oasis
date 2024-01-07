3c3c279f4849b1d049abf30771724e040b19f780
LULIU-9 <lu.liu4work@gmail.com>
2024-01-07 21:14:48 +1000

    feat: implement dark mode style

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 import ButtonIcon from "./ButtonIcon";
-import { HiOutlineUser } from "react-icons/hi2";
+import { HiOutlineMoon, HiOutlineSun, HiOutlineUser } from "react-icons/hi2";
 import Logout from "../features/authentication/Logout";
 import { useNavigate } from "react-router-dom";
 import UserAvatar from "../features/authentication/UserAvatar";
+import { useDarkMode } from "../context/DarkModeContext";
 
 const StyledHeaderMenu = styled.ul`
   display: flex;
@@ -12,6 +13,8 @@ const StyledHeaderMenu = styled.ul`
 
 function HeaderMenu() {
   const navigate = useNavigate();
+
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
   return (
     <StyledHeaderMenu>
       <li>
@@ -24,6 +27,11 @@ function HeaderMenu() {
       </li>
       <li>
         <Logout />
+      </li>
+      <li>
+        <ButtonIcon onClick={toggleDarkMode}>
+          {isDarkMode ? <HiOutlineSun /> : <HiOutlineMoon />}
+        </ButtonIcon>
       </li>
     </StyledHeaderMenu>
   );
